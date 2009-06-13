bb44154cea0f29c20bb7bed54c543f20eda3d357
Cliff Hall <cliff.hall@puremvc.org>
2009-06-13 16:38:52 +0000

    updated email and maintainer

--- src/org/puremvc/java/multicore/patterns/command/MacroCommand.java
+++ src/org/puremvc/java/multicore/patterns/command/MacroCommand.java
@@ -1,7 +1,6 @@
 /* 
  PureMVC Java MultiCore Port by Ima OpenSource <opensource@ima.eu>
- Maintained by Matthieu Mauny <matthieu.mauny@puremvc.org>
- And Anthony Quinault <aquinault@gmail.com>
+ Maintained by Anthony Quinault <anthony.quinault@puremvc.org>
  PureMVC - Copyright(c) 2006-08 Futurescale, Inc., Some rights reserved. 
  Your reuse is governed by the Creative Commons Attribution 3.0 License 
  */
